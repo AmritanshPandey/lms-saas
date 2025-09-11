792cb9b4f1b7c74bb722721bf49358cf27fdbe22
Amritansh Pandey <amritansh.pandey6@gmail.com>
2025-09-11 11:17:16 +0530

    routing done

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,8 @@
+const CompanionSesssion = () => {
+  return (
+    <div>CompanionSesssion</div>
+  )
+
+}
+
+export default CompanionSesssion
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello, Next.js!</h1>
+      <p>This is a sample page using Tailwind CSS.</p>
+      <Button>Get started</Button>
+    </div>
   )
 }
 
